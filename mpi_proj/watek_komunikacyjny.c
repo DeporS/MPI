76273ecd0f27ea8df05b89705750e4e864c56143
DeporS <depores1337@gmail.com>
2024-06-06 16:04:45 +0200

    dodanie lokalnego counta

--- mpi_proj/watek_komunikacyjny.c
+++ mpi_proj/watek_komunikacyjny.c
@@ -152,6 +152,7 @@ void *startKomWatek(void *ptr)
             }
 
             THE_END_counter++;
+            printf("THE END COUNTER %d\n", THE_END_counter);
             if (THE_END_counter == min(killer_count_local, victim_count_local))
             {
                 for (int i = 0; i < size; i++)
